3a828b6c9f2bc8275c4487c9a5e3cba4c93dc14e
aggelikitsek1-lang <aggelikitsek1@gmail.com>
2026-09-07 17:40:03 +0200

    Update 04_task_h_event_study.R

--- code/04_task_h_event_study.R
+++ code/04_task_h_event_study.R
@@ -23,4 +23,7 @@ event_windows <- list(
 )
 
 # The event window [-1, +1] and [-2, +2] are defined relative to the referendum result, which was announced on Friday, 24 June 2016. Since weekends and holidays are not considered trading days, for the event window [-1, +1], -1 falls on 23 June 2016 (Thursday) and +1 falls on 27 June 2016 (Monday, since the weekend of 25-26 June is skipped). Similarly, for the vent window [-2, +2], -2 falls on 28 June 2016 (Tuesday).  
-# 2. 
+
+# 2. Sample composition
+
+
