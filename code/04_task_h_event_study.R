d59db55b7eb272bec9eccd86efe439f163197d45
aggelikitsek1-lang <aggelikitsek1@gmail.com>
2026-09-07 17:22:33 +0200

    Update 04_task_h_event_study.R

--- code/04_task_h_event_study.R
+++ code/04_task_h_event_study.R
@@ -1,7 +1,7 @@
 # =======================================================
 # Empirical Research in Finance - Group Assignment
 #
-# Purpose:
+# Purpose: A comparison of the stock price reaction to the Brexit referendum between UK-exposed and non-exposed German firms.
 # Input:
 # Output:
 # =======================================================
@@ -14,7 +14,7 @@ load("data/clean/value_effect_panel_clean.rda")
 #Load event-study summary file (no cleaning needed)
 cars <- haven::read_dta("data/raw/cars_summary.dta") |> haven::zap_label()
 
-# 1 . Event design 
+# 1. Event design 
 
 event_date <- as.Date("2016-06-24") # Brexit announcement 
 event_windows <- list(
@@ -23,3 +23,4 @@ event_windows <- list(
 )
 
 # The event window [-1, +1] and [-2, +2] are defined relative to the referendum result, which was announced on Friday, 24 June 2016. Since weekends and holidays are not considered trading days, for the event window [-1, +1], -1 falls on 23 June 2016 (Thursday) and +1 falls on 27 June 2016 (Monday, since the weekend of 25-26 June is skipped). Similarly, for the vent window [-2, +2], -2 falls on 28 June 2016 (Tuesday).  
+# 2. 
